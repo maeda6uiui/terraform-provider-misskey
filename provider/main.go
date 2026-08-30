6a6b6459a4c4196da0728f5fc8b4c19a381ccae8
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2026-08-30 21:07:42 +0900

    Add main.go

---
+++ provider/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/maeda6uiui/terraform-provider-misskey/internal/provider"
+)
+
+var (
+	version string = "0.0.1-alpha1"
+)
+
+func main() {
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := providerserver.ServeOpts{
+		Address: "registry.terraform.io/maeda6uiui/misskey",
+		Debug:   debug,
+	}
+	err := providerserver.Serve(context.Background(), provider.New(version), opts)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
